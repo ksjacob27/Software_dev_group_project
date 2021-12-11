139438576aef86ed7af357fac8c227a34756facf
MicMetz <47361643+MicMetz@users.noreply.github.com>
2021-12-11 03:22:14 -0700

    Add files via upload

---
+++ UI/PlayerGUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerGUI : MonoBehaviour {
+    public Text playerName;
+
+    public void SetPlayerInfo(PlayerInfo info) {
+        playerName.text = $"Player {info.playerId}";
+        playerName.color = info.ready ? Color.green : Color.red;
+    }
+}
---
+++ UI/SpellTooltip.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+
+/*
+ * Attempt to offer a World Of Warcraft style actionbar abilities template.
+ * (need to broaden to include physical abilities, and consumables. 
+ */
+public class SpellTooltip : MonoBehaviour {
+    public bool     castable;
+    public Image    thumbnail;
+    public Image    selectedHighlight;
+    public TMP_Text manaCostText;
+
+    
+    
+    public void EmplaceAction(SpellScript script, bool canCast) {
+        this.thumbnail.sprite = script._SpellSprite;
+        this.manaCostText.text = script.ManaCost.ToString();
+        castable = canCast;
+    }
+
+    
+    public void Selected(bool state) {
+        selectedHighlight.gameObject.SetActive(state);
+    }
+}
